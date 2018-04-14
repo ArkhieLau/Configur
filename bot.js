74e9fe3776eca9776822f4a5d566bc6f327ae6e0
ArkhieLau <38354687+ArkhieLau@users.noreply.github.com>
2018-04-15 00:58:55 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ if(cmd === `${prefix}checkwarns`){
     let wlevel = cwarns[CwUser.id].warns;
 
 
-    message.channel.send(`<@${CwUser.id}> has ${cwarns} warns`)
+    message.reply(`<@${CwUser.id}> has ${cwarns} warns`);
 }
 if(cmd === `${prefix}warn`){
     let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
